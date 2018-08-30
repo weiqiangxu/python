1e42bf675bea34df0ca5587361089f7b7fa79e7e
xuweiqiang <435861851@qq.com>
2018-08-31 06:21:48 +0800

    学习笔记本-为了更稳健、高效的爬虫

---
+++ 0831.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#输入和输出
+print('hello world')
+print('xuweiqiang', 'is', 'cool')
+#算数运算
+print('100 + 200 =', 100 + 200)
+#输入
+print('please Enter you name:')
+name=input()
+print('hello,the rich man',name)
