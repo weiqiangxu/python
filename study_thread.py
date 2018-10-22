3c1a379db0ae17d88fd71e406bd7926d7a98d55c
435861851@qq.com <435861851@qq.com>
2018-10-22 14:18:15 +0800

    学习python的多线程编程，争取尽快写个多线程的小爬虫

---
+++ study_thread.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+
+import _thread
+import time
+
+# 为线程定义一个函数
+def print_time( threadName, delay):
+   count = 0
+   while count < 5:
+      time.sleep(delay)
+      count += 1
+      print ("%s: %s" % ( threadName, time.ctime(time.time()) ))
+
+# 创建两个线程
+# try:
+#    _thread.start_new_thread( print_time, ("Thread-1", 1, ) )
+#    _thread.start_new_thread( print_time, ("Thread-2", 2, ) )
+# except:
+#    print ("Error: 无法启动线程")
+
+# while 1:
+#    pass
+
+
+
+#!/usr/bin/python3
+
+import threading
+import time
+
+class myThread (threading.Thread):
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+    def run(self):
+        print ("开启线程： " + self.name)
+        # 获取锁，用于线程同步
+        threadLock.acquire()
+        print_time(self.name, self.counter, 3)
+        # 释放锁，开启下一个线程
+        threadLock.release()
+
+def print_time(threadName, delay, counter):
+    while counter:
+        time.sleep(delay)
+        print ("%s: %s" % (threadName, time.ctime(time.time())))
+        counter -= 1
+
+threadLock = threading.Lock()
+threads = []
+
+# 创建新线程
+thread1 = myThread(1, "Thread-1", 1)
+thread2 = myThread(2, "Thread-2", 2)
+
+# 开启新线程
+thread1.start()
+thread2.start()
+
+# 添加线程到线程列表
+threads.append(thread1)
+threads.append(thread2)
+
+# 等待所有线程完成
+for t in threads:
+    t.join()
+print ("退出主线程")
