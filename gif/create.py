542e2ff7ac1ee4d3c07a55ec1c7526b8588edbe0
435861851@qq.com <435861851@qq.com>
2018-11-15 18:31:12 +0800

    更新生成gif的function

--- gif/create.py
+++ gif/create.py
@@ -1,8 +1,10 @@
+#!/bin/python3
+
 from PIL import Image
 import sys
 import matplotlib.pyplot as plt
 import imageio,os
-
+import re
 # gif图片生成多张png
 def processImage(infile):
     try:
@@ -12,29 +14,35 @@ def processImage(infile):
         sys.exit(1)
     i = 0
     mypalette = im.getpalette()
-
     try:
         while 1:
             im.putpalette(mypalette)
             new_im = Image.new("RGBA", im.size)
             new_im.paste(im)
             new_im.save('a'+str(i)+'.png')
-
             i += 1
             im.seek(im.tell() + 1)
-
     except EOFError:
         pass # end of sequence
 
 # processImage('wangjingze.gif')
 
 
-# 多张png图片生成gif
-def create_gif(save_file):
-    images = []
-    filenames=sorted((fn for fn in os.listdir('.') if fn.endswith('.png')))
-    for filename in filenames:
-        images.append(imageio.imread(filename)) 
-    imageio.mimsave(save_file, images,duration=1)
+# 将文件夹内部所有png转为gif
+def create_gif(image_dir, gif_name):
+    image_list=[]
+    # 循环文件夹
+    for img in os.listdir(image_dir):
+        if(img.endswith('.png')):
+            image_list.append(image_dir+'/'+img)
+    print(image_list)
+    # gif源文件列表
+    frames = []
+    for image_name in image_list:
+        frames.append(imageio.imread(image_name))
+    # Save them as frames into a gif
+    imageio.mimsave(gif_name, frames, 'GIF', duration = 0.1)  
+    return
 
-# create_gif('new.gif')
+
+create_gif('./image','new.gif' )
